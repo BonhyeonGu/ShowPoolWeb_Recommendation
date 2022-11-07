81f45b54af76bbfec5ca43d404ad451636f0d539
whwhdtk <8whwhdtk8@naver.com>
2022-11-07 11:20:10 +0900

    초기화 갱신 안되게 해놓음

--- app.py
+++ app.py
@@ -15,24 +15,28 @@ userLastClick = dict()
 #-------------------------------------------------------------
 SLEEP_SEC = 5
 REF_NUM = 3 #참고할 영상 개수, 웹사이트와 동일하게 할 것
-LOACATION_TITLE2ID = "C:/Users/MY/.vscode/tt/Wikification_web/Wikification_web/ComTittleToID.pkl"
-LOACATION_BACKLINKS = "C:/Users/MY/.vscode/tt/Wikification_web/Wikification_web/backlinks/"
+LOACATION_TITLE2ID = "C:/Users/8whwh/Git/Reasoning_over_Knowledge_Component_Streams/ComTittleToID.pkl"
+LOACATION_BACKLINKS = "C:/Users/8whwh/Git/Reasoning_over_Knowledge_Component_Streams/backlinks/"
 #-------------------------------------------------------------
 
 def dbInit():
     oldCol = doc
     oldCol.drop()
     newCol = doc
+
+    nowDate = datetime.today().strftime("%Y%m%d%H%M%S")
     for i in range(1, 4):
         user = {
             "id" : "user0%d" % (i),
             "pw" : "1234",
             "clickedID" : (),
-            "recommID" : (),
-            "time_lastclick" : "x",
+            "recommID1" : (),
+            "recommID2" : (),
+            "time_lastclick" : nowDate,
             "time_lastupdate" : "x"
         }
         newCol.insert_one(user)
+        userLastClick["user0%d" % (i)] = nowDate
 
 def mainRoutine():
     with open(LOACATION_TITLE2ID,'rb') as f:
@@ -45,15 +49,17 @@ def mainRoutine():
                 allID, allData = Neo().runAll()
                 #---------------------------------------------------------------------------------------------
                 recomm = Recomm(title2Id, LOACATION_BACKLINKS, allID, allData, user_info['clickedID']).run()
-                doc.update_one({"id" : user_info['id']}, {"$set":{"recommID" : recomm}})
+                doc.update_one({"id" : user_info['id']}, {"$set":{"recommID1" : recomm}})
+                recomm = Recomm(title2Id, LOACATION_BACKLINKS, allID, allData, user_info['clickedID']).RWR()
+                doc.update_one({"id" : user_info['id']}, {"$set":{"recommID2" : recomm}})
                 #---------------------------------------------------------------------------------------------
                 userLastClick[user_info['id']] = user_info['time_lastclick']
                 nowDate = datetime.today().strftime("%Y%m%d%H%M%S")
                 doc.update_one({"id" : user_info['id']}, {"$set":{"time_lastupdate" : nowDate}})
-                print("%s is updated, process %dsecond sleep"%(user_info['id'], SLEEP_SEC))
-                time.sleep(SLEEP_SEC)
             else:
                 print("%s is not changed, update skip"%(user_info['id']))
+            print("process %dsecond sleep"%(SLEEP_SEC))
+            time.sleep(SLEEP_SEC)
 
 def main():
     mainRoutine()
